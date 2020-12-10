8a8f90172e7be75192c31487bad4fa933d753a17
DongKyu <a29661498@gmail.com>
2020-12-10 15:18:43 +0900

    Add files via upload

---
+++ Test/13-5.js
@@ -0,0 +1,15 @@
+var grade;
+onmessage=function(e){
+	var score=e.data;
+	if(score>=90&&score<=100){
+		grade="A";
+	}else if(score>=80){
+		grade="B";
+	}else if(score>=70){
+		grade="C";
+	}else if(score>=60){
+		grade="D";
+	}else{
+		grade="F";
+	}
+}
